c7cf07acff91b4fc3451172d097f76a6e1412ce7
Mi1an0 <milan9927kumar@gmail.com>
2022-01-11 13:52:42 +0530

    11 jan update

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const sdiv = document.getElementsByClassName('skilldiv');
-const m = document.querySelector('h1');
-const n = document.querySelector('.navbar-area');
+const sdiv = document.getElementsByClassName('skilldiv')
+const m = document.querySelector('h1')
+const n = document.querySelector('.navbar-area')
+const hamburger = document.querySelector('.hamburger')
+const pages = document.querySelector('.pages')
 
 function hopup(){
-    this.style.marginBottom = 50 +"px";
-    // setTimeout(() => this.style.marginBottom = 0,300);
+    this.style.marginBottom = 50 +"px"
 }
 
 function hopdown(){
-    this.style.marginBottom = 0;
+    this.style.marginBottom = 0
 }
 
 for(const x of sdiv){
-    x.addEventListener("mouseover",hopup);
+    x.addEventListener("mouseover",hopup)
     x.addEventListener("mouseout",hopdown)
 }
 
 m.addEventListener("mouseover", function(){
-    m.style.fontSize = 80 +"px";
-    n.style.display = "none";
-});
+    m.style.fontSize = 80 +"px"
+    n.style.display = "none"
+})
 
 m.addEventListener("mouseout", function(){
-    m.style.fontSize = 60 +"px";
-    n.style.display = "flex";
-});
+    m.style.fontSize = 60 +"px"
+    n.style.display = "flex"
+})
+
+hamburger.addEventListener("click", function(){
+    pages.classList.toggle('active')
+})
